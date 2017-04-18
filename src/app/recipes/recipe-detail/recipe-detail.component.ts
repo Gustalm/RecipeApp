6a0b7ff212c3dc5b268846a67cfaa6a661bf00cf
Gustavo Almeida <gustatec@gmail.com>
2017-04-18 18:08:18 -0300

    [Add] params alternative method to get data from url

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
 import { ShoppingListService } from "app/shopping-list/shopping-list.service";
-import { ActivatedRoute, Data } from "@angular/router";
+import { ActivatedRoute, Data, Params } from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -17,9 +17,14 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.data.subscribe(
-      (data: Data) => {
-        this.recipe = data['recipe'];
+    // usando resolver
+    // this.route.data.subscribe(
+    //   (data: Data) => {
+    //     this.recipe = data['recipe'];
+    //   }) 
+
+      this.route.params.subscribe((data: Params) => {
+        this.recipe = this.recipeService.getRecipeById(+data['id']);
       })
   }
 
