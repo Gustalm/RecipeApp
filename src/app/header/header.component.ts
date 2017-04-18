f2f4abcf18cb46bcea909016d0c6e15dd14b7128
Gustavo Almeida <gustatec@gmail.com>
2017-04-18 18:10:12 -0300

    a

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,15 +7,8 @@ import { Menu } from '../menu.enum';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() selectedMenu = new EventEmitter<number>();
-  header = Menu;
-
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onMenuSelect(idMenu: number) {
-    this.selectedMenu.emit(idMenu);
   }
 }
